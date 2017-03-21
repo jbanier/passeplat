035d2c08b32049f32d7209a63d6426dc31104ceb
Jérémie Banier <jbanier@gmail.com>
2017-03-21 16:12:44 +0100

    add proper clientid to avoid confliciting with other apps

--- passeplat.go
+++ passeplat.go
@@ -13,7 +13,7 @@ import (
 var (
 	addr        = flag.String("addr", "0.0.0.0:514", "The address to bind to")
 	brokers     = flag.String("brokers", os.Getenv("KAFKA_PEERS"), "The Kafka brokers to connect to, as a comma separated list")
-	topic	    = flag.String("topic", "syslog-raw", "The name of the target topic")
+	topic       = flag.String("topic", "syslog-raw", "The name of the target topic")
 	verbose     = flag.Bool("verbose", false, "Turn on Sarama logging")
 	emitjson    = flag.Bool("json", true, "preparse syslog message in syslog format, only parse the syslog fields")
 	echomessage = flag.Bool("echo", false, "echo the parsed message on stdout")
@@ -46,33 +46,40 @@ func main() {
 		log.Printf("Failed to listen on UDP on port %s, %s", *addr, err)
 		os.Exit(1)
 	}
+	err = server.ListenTCP(*addr)
+	if err != nil {
+		log.Printf("Failed to listen on UDP on port %s, %s", *addr, err)
+		os.Exit(1)
+	}
 	server.Boot()
 	dataCollector := newDataCollector(brokerList)
 
-	go func(channel syslog.LogPartsChannel) {
-		for logParts := range channel {
-			var encoded, err = json.Marshal(logParts)
-			if *echomessage {
-				log.Println(string(encoded))
-			}
-			if err != nil {
-				log.Printf("Failed to Marshal message, %s", err)
-			}
-			// We are not setting a message key, which means that all messages will
-			// be distributed randomly over the different partitions.
-			partition, offset, err := dataCollector.SendMessage(&sarama.ProducerMessage{
-				Topic: *topic,
-				Value: sarama.StringEncoder(encoded),
-			})
+	for i := 0; i <= 5; i++ {
+		go func(channel syslog.LogPartsChannel) {
+			for logParts := range channel {
+				var encoded, err = json.Marshal(logParts)
+				if *echomessage {
+					log.Println(string(encoded))
+				}
+				if err != nil {
+					log.Printf("Failed to Marshal message, %s", err)
+				}
+				// We are not setting a message key, which means that all messages will
+				// be distributed randomly over the different partitions.
+				partition, offset, err := dataCollector.SendMessage(&sarama.ProducerMessage{
+					Topic: *topic,
+					Value: sarama.StringEncoder(encoded),
+				})
 
-			if err != nil {
-				log.Printf("Failed to store your data:, %s", err)
-			} 
-			if *verbose {
-				log.Printf("stored with part %d offset %d\n", partition, offset)
+				if err != nil {
+					log.Printf("Failed to store your data:, %s", err)
+				}
+				if *verbose {
+					log.Printf("stored with part %d offset %d\n", partition, offset)
+				}
 			}
-		}
-	}(channel)
+		}(channel)
+	}
 
 	server.Wait()
 	log.Println("after wait")
@@ -87,6 +94,7 @@ func newDataCollector(brokerList []string) sarama.SyncProducer {
 	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
 	config.Producer.Retry.Max = 10                   // Retry up to 10 times to produce the message
 	config.Producer.Return.Successes = true
+	config.Producer.ClientID = "passeplat"
 	// On the broker side, you may want to change the following settings to get
 	// stronger consistency guarantees:
 	// - For your broker, set `unclean.leader.election.enable` to false
